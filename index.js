a74868d97f02be45e57229a3a2a0762eb8c35844
Milos Dzeletovic <miloshdz@gmail.com>
2018-05-26 17:52:25 +0200

    The Reducer function

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+
 {
   type: 'ADD_TODO',
   todo: {
@@ -28,6 +29,21 @@
 {
   type: 'REMOVE_GOAL',
   id: 0
+}
+
+/*
+  Characteristics of a Pure Function
+  1) They always return the same result if the same arguments are passed in.
+  2) They depend only on the arguments passed into them.
+  3) Never produce any side effects (No interaction with the outside world - Ajax requests, DOM interaction...)
+*/
+
+function todos (state = [], action) {
+  if (action.type === 'ADD_TODO') {
+    return state.concat([action.todo])
+  }
+
+  return state
 }
 
 
