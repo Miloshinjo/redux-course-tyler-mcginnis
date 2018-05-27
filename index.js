166f06252d64c66c40bef43e535e0a9d4fbe4409
Milos Dzeletovic <miloshdz@gmail.com>
2018-05-27 02:01:43 +0200

    Combine Reducers

--- index.js
+++ index.js
@@ -37,7 +37,7 @@
   3) Never produce any side effects (No interaction with the outside world - Ajax requests, DOM interaction...)
 */
 
-// Reducer function
+// Reducer functions
 function todos (state = [], action) {
   switch(action.type) {
     case 'ADD_TODO':
@@ -50,6 +50,25 @@ function todos (state = [], action) {
       return state
   }
 }
+
+function goals (state = [], action) {
+  switch (action.type) {
+    case 'ADD_GOAL':
+      return state.concat([action.goal])
+    case 'REMOVE_GOAL':
+      return state.filter((goal) => goal.id !== action.id)
+    default:
+      return state
+  }
+}
+
+function app (state = {}, action) {
+  return {
+    todos: todos(state.todos, action),
+    goals: goals(state.goals, action)
+  }
+}
+
 
 function createStore(reducer) {
   // The store should have 4 parts
@@ -85,4 +104,66 @@ function createStore(reducer) {
   }
 }
 
-const store = createStore(todos)
+const store = createStore(app)
+
+store.subscribe(() => {
+  console.log('The new state is: ', store.getState())
+})
+
+store.dispatch({
+  type: 'ADD_TODO',
+  todo: {
+    id: 0,
+    name: 'Walk the dog',
+    complete: false,
+  }
+})
+
+store.dispatch({
+  type: 'ADD_TODO',
+  todo: {
+    id: 1,
+    name: 'Wash the car',
+    complete: false,
+  }
+})
+
+store.dispatch({
+  type: 'ADD_TODO',
+  todo: {
+    id: 2,
+    name: 'Go to the gym',
+    complete: true,
+  }
+})
+
+store.dispatch({
+  type: 'REMOVE_TODO',
+  id: 1
+})
+
+store.dispatch({
+  type: 'TOGGLE_TODO',
+  id: 0
+})
+
+store.dispatch({
+  type: 'ADD_GOAL',
+  goal: {
+    id: 0,
+    name: 'Learn Redux'
+  }
+})
+
+store.dispatch({
+  type: 'ADD_GOAL',
+  goal: {
+    id: 1,
+    name: 'Lose 20 pounds'
+  }
+})
+
+store.dispatch({
+  type: 'REMOVE_GOAL',
+  id: 0
+})
